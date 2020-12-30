8179ec458078b88ee80f3741223fea42b7dc2f72
dracero <diego.racero@gmail.com>
2020-12-30 12:53:28 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-)
-
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":3000", nil))
-}
-
-
+	"fmt"
+ 	"log"
+ 	"net/http"
+ 	"os"
+ )
+ 
 func main() {
-    handleRequests()
+http.HandleFunc("/", handler)
+fmt.Println("listening...")
+err := http.ListenAndServe(GetPort(), nil)
+if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+ }
 }
+ 
+func handler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "Hello. This is our first Go web app on Heroku!")
+}
+ 
+// Get the Port from the environment so we can run on Heroku
+func GetPort() string {
+var port = os.Getenv("PORT")
+// Set a default port if there is nothing in the environment
+if port == "" {
+		port = "4747"
+ 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+ 	}
+ 	return ":" + port
+ }
